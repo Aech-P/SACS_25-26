9c9636e55900441b322e15d715102888d3640510
Aech-P <honraoprutha17@gmail.com>
2026-06-09 12:14:03 +0530

    Update rondo.js

--- rondo.js
+++ rondo.js
@@ -1,6 +1,6 @@
 /* Load site/pages/items from Google Sheets (gviz) and render site.
    Items list is independent of pages: no auto-filtering by page. */
-var spreadsheetID = "1iwIx8sqiBTqbtAI7TX4vDGDVee3dPz0wa3MazBv2fzQ";
+var spreadsheetID = "1EVvVT1bD5vevac-cE221CbkHT00BZnZD";
 var siteSheet  = "1569296108";
 var pagesSheet = "28080804";
 var itemsSheet = "0";
